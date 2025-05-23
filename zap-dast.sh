ce6ce41863552d284f54e715bd1fd995bcf5c24a
sairam18011996 <113753090+sairam18011996@users.noreply.github.com>
2025-05-23 18:12:58 +0530

    Update zap-dast.sh

--- zap-dast.sh
+++ zap-dast.sh
@@ -4,13 +4,19 @@ set -e
 ZAP_REPORT_DIR="zap-report"
 mkdir -p $ZAP_REPORT_DIR
 
-TARGET_URL=$TARGET_URL
+# Sairam's staging URL
+TARGET_URL="http://devsecopsgenai-staging.eba-3u9au2bw.us-east-1.elasticbeanstalk.com/"
+
+echo "🔍 Running ZAP scan on: $TARGET_URL"
 
 docker run --user root \
   -v $(pwd)/$ZAP_REPORT_DIR:/zap/wrk/:rw \
   ghcr.io/zaproxy/zaproxy:stable zap-baseline.py \
   -t "$TARGET_URL" \
   -r zap-report.html || true
+
+# Move the report to correct folder for CodeBuild
+mv zap-report.html $ZAP_REPORT_DIR/zap-report.html
 
 echo "📁 Listing report folder contents..."
 ls -lh $ZAP_REPORT_DIR
